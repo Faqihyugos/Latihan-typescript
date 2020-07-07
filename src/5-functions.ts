38d1fe1b3c9faf7f313d77c4ba0d86f410cb3aeb
Faqihyugos <faqihyugos@gmail.com>
2020-07-08 01:20:01 +0700

    latihan function

--- src/5-functions.ts
+++ src/5-functions.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prefer-const */
 
@@ -15,25 +16,31 @@ export {}; // quick fix for global variable
  */
 
 // function declaration
-// function add(x, y) {
-//     return x + y;
-// }
+function add(x: number, y: number, z?: number, a = 0): number {
+    return x + y + a;
+}
+
+console.log(add(2, 3));
 
 // function expression
-// const addEx = function (x, y) {
-//     return x + y;
-// };
+const addEx = function (x: number, y: number) {
+    return x + y;
+};
 
 // arrow function
-// const addArrow = (x, y) => x + y;
+const addArrow = (x: number, y: number): number => x + y;
 
 // function with no return value
-// const hello = (name) => console.log("hello " + name)
+const hello = (name: string) => console.log('hello ' + name);
 
+type Greeter = (message: string) => void;
 // function with callback
-// function request(url,cb){
+function request(url: string, cb: Greeter) {
+    cb(url);
+}
 
-// }
+let fn = (str: string) => console.log(str);
+request('http://typescriptlang.org', fn);
 
 /********** 2. optional & default parameters  ***********/
 /*
@@ -45,24 +52,32 @@ export {}; // quick fix for global variable
 | languange  | No       | english       |
 */
 
-/*
-function submitContact(firstName, lastName, languange, gender){
-  return {
-    firstName,
-    lastName
-    languange
-  }
+type Contact = {
+    firstName: string;
+    lastName: string;
+    gender?: string;
+    language: string;
+};
+
+function submitContact(firstName: string, lastName: string, language = 'english', gender?: string): Contact {
+    return {
+        firstName: firstName,
+        lastName: lastName,
+        language,
+        ...(gender && { gender }),
+    };
 }
-*/
+
+let result = submitContact('faqih', 'yugo', 'indonesia', 'male');
+console.log(result);
 
 /********** 3. Rest Parameter  ***********/
-/*
-function fruitsCollection(item, ...restItems) {
-  return item + " " + restItems.join(" ");
+
+function fruitsCollection(item: string, ...restItems: string[]) {
+    return item + ' ' + restItems.join(' ');
 }
 let fruits = fruitsCollection('Apple', 'Mango', 'Avocado');
-// console.log(fruits);
-*/
+console.log(fruits);
 
 /********* 4. conditional type with union ***********/
 /*
@@ -70,6 +85,27 @@ let fruits = fruitsCollection('Apple', 'Mango', 'Avocado');
   - return either string or number
   - don't use `any`
 */
-// function CT(param) {
-//   return param
-// }
+
+// solution 1 : union
+function CT(param: string | number) {
+    return param;
+}
+
+// solution 2: generic
+// type variable -> deteksi type data dari caller
+function CT_generic<T>(param: T): T {
+    return param;
+}
+
+CT_generic<string>('hello');
+CT_generic<number>(112);
+
+//   solution 3: overloading
+function CT_overload(param: string): string;
+function CT_overload(param: number): number;
+function CT_overload(param: any): any {
+    return param;
+}
+CT_overload('hello');
+CT_overload(123);
+// CT_overload(false);
